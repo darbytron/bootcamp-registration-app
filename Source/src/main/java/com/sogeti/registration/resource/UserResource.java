b7b246343be767f60ccb7dfcfcfac3f7bda2da0e
Jordan Bossman <jbossman5@gmail.com>
2015-02-05 18:23:35 -0500

    Added update user and update event

--- Source/src/main/java/com/sogeti/registration/resource/UserResource.java
+++ Source/src/main/java/com/sogeti/registration/resource/UserResource.java
@@ -6,14 +6,19 @@
 package com.sogeti.registration.resource;
 
 import java.util.List;
+
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import com.sogeti.registration.beans.User;
 import com.sogeti.registration.hibernate.HibernateUtil;
 import com.sogeti.registration.service.UserService;
@@ -79,6 +84,15 @@ public class UserResource
 		}
 
 		return null;
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User updateUser(User user) throws Exception
+	{
+		HibernateUtil.saveOrUpdate(user);
+		return user;
 	}
 	
 	//Creates a user when served User json

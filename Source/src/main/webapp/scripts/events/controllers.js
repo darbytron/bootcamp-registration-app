94ccff723af5495ec741bb55604ae419d7cda6a8
Alex Krebiehl <alex.krebiehl@gmail.com>
2015-02-04 22:50:45 -0500

    Feature complete edit/update event

--- Source/src/main/webapp/scripts/events/controllers.js
+++ Source/src/main/webapp/scripts/events/controllers.js
@@ -2,7 +2,7 @@
  * Created by Alex on 2/3/15.
  */
 
-const SERVER_ADDRESS = "http://localhost:8080/Registration/";
+const SERVER_ADDRESS = "http://localhost:8080/Registration";
 //const SERVER_ADDRESS = "http://javabootcamp01.sgt.com:8080/Registration";
 
 var event = angular.module('Registration', ['ngRoute']);
@@ -21,11 +21,11 @@ event.config(['$routeProvider', function($routeProvider) {
         })
         .when("/event/edit/:eventId", {
             templateUrl : "views/events/editevent.html",
-            controller : "EventEditController"
+            controller : "EditEventController"
         })
         .when("/event/new", {
             templateUrl : "views/events/editevent.html",
-            controller : "EventEditController"
+            controller : "EditEventController"
         })
         // The same controller will be used for editing and creating
 
@@ -101,39 +101,43 @@ event.controller('EventViewController', [ '$scope', '$location', '$routeParams',
     }]);
     */
 
-event.controller('EditEventController', ['$scope', '$location', '$routeParams', function($scope, $location, $routeParams){
-
-    /*
-    $scope.event = {
-        "id" : 1,
-        "title" : "test event",
-        "startDate" : "10-24",
-        "endDate" : "12-5",
-        "category" : "some category",
-        "type" : "some type",
-        "description" : "blah blah blah",
-        "owner" : "Krebiehl, Alex",
-        "location" : "Dayton, OH",
-        "status" : "Active",
-        "registrations" : [
-            { "firstName": "Tyler", "lastname": "Darby", "id": 7},
-            { "firstName": "Jordan", "lastname": "Bossman", "id": 8},
-            { "firstName": "Tyler", "lastname": "Robertson", "id": 9}
-        ],
-        "food" : [
-            "Carrots",
-            "Peas"
-        ]
-    };
+event.controller('EditEventController', ['$scope', '$location', '$routeParams', '$http', function($scope, $location, $routeParams, $http){
 
 
-
-    // $scope.getEvent($routeParams.eventId);
-    $scope.pageTitle = $scope.event.title;
 
     // If eventId is set, we are editing, otherwise it is a new event
-    if ($routeParams.eventId)
+    if ($routeParams.eventId != null)
     {
+
+
+        /* test data */
+         $scope.event = {
+         "id" : 1,
+         "title" : "test event",
+         "startDate" : "10-24",
+         "endDate" : "12-5",
+         "category" : "some category",
+         "type" : "some type",
+         "description" : "blah blah blah",
+         "owner" : "Krebiehl, Alex",
+         "location" : "Dayton, OH",
+         "status" : "Active",
+         "registrations" : [
+         { "firstName": "Tyler", "lastname": "Darby", "id": 7},
+         { "firstName": "Jordan", "lastname": "Bossman", "id": 8},
+         { "firstName": "Tyler", "lastname": "Robertson", "id": 9}
+         ],
+         "food" : [
+         "Carrots",
+         "Peas"
+         ]
+         };
+
+
+
+
+
+        $scope.mainPanelTitle = "Edit Event";
         // Retrieve event details to edit
         $scope.getEvent = function(eventId) {
             $http.get(SERVER_ADDRESS + '/resource/event/'+ eventId).
@@ -145,7 +149,39 @@ event.controller('EditEventController', ['$scope', '$location', '$routeParams', 
                 });
         };
     }
+    else
+    {
+        $scope.mainPanelTitle = "Create Event";
+    }
 
-*/
 
+    $scope.updateEvent = function() {
+
+        if ($scope.event.id)
+        {
+            // We have an eventId, so we are editing an event
+            $http.put(SERVER_ADDRESS + 'resource/event/' + $scope.event.id, $scope.event).
+                success(function(data, status, headers, config) {
+
+                    // navigate to event view page
+                    $location.path('/event/' + $scope.event.id);
+                }).
+                error(function(data, status, headers, config) {
+                    console.log( data );
+                });
+        }
+        else
+        {
+            // no event id, so we are creating
+            $http.post(SERVER_ADDRESS + 'resource/event/', $scope.event).
+                success(function(data, status, headers, config) {
+
+                    // navigate to event view page
+                    $location.path('/event/' + $scope.event.id);
+                }).
+                error(function(data, status, headers, config) {
+                    console.log( data );
+                });
+        }
+    };
 }]);

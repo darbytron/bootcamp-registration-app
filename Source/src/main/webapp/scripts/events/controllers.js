5c36d66721c9bcb55e6fa966fc01a18a4e6ce6f8
Alex Krebiehl <alex.krebiehl@gmail.com>
2015-02-05 13:39:10 -0500

    Added buttons to myEvents

--- Source/src/main/webapp/scripts/events/controllers.js
+++ Source/src/main/webapp/scripts/events/controllers.js
@@ -26,7 +26,7 @@ event.config(['$routeProvider', function($routeProvider) {
         .when("/event/new", {
             templateUrl : "views/events/editevent.html",
             controller : "EditEventController"
-        })
+        });
         // The same controller will be used for editing and creating
 
         /*
@@ -35,7 +35,7 @@ event.config(['$routeProvider', function($routeProvider) {
             controller : "EditEventController"
         })
          */
-        .otherwise({redirectTo: '/'});
+        //.otherwise({redirectTo: '/'});
 }]);
 
 
@@ -50,6 +50,74 @@ event.config(['$routeProvider', function($routeProvider) {
 event.controller('MyEventsController', ['$scope', '$location', function($scope, $location){
         $scope.pageTitle = "My Events";
 
+    $scope.events = [
+        {
+        "id" : 1,
+        "title" : "test event1",
+        "startDate" : "10-24",
+        "endDate" : "12-5",
+        "category" : "some category",
+        "type" : "some type",
+        "description" : "blah blah blah",
+        "owner" : "Krebiehl, Alex",
+        "location" : "Dayton, OH",
+        "status" : "Active",
+        "registrations" : [
+            { "firstName": "Tyler", "lastname": "Darby", "id": 7},
+            { "firstName": "Jordan", "lastname": "Bossman", "id": 8},
+            { "firstName": "Tyler", "lastname": "Robertson", "id": 9}
+        ],
+        "food" : [
+            "Carrots",
+            "Peas"
+        ]
+    },
+        {
+            "id" : 2,
+            "title" : "test event2",
+            "startDate" : "10-24",
+            "endDate" : "12-5",
+            "category" : "some category",
+            "type" : "some type",
+            "description" : "blah blah blah",
+            "owner" : "Krebiehl, Alex",
+            "location" : "Dayton, OH",
+            "status" : "Active",
+            "registrations" : [
+                { "firstName": "Tyler", "lastname": "Darby", "id": 7},
+                { "firstName": "Jordan", "lastname": "Bossman", "id": 8},
+                { "firstName": "Tyler", "lastname": "Robertson", "id": 9}
+            ],
+            "food" : [
+                "Carrots",
+                "Peas"
+            ]
+        },
+        {
+            "id" : 3,
+            "title" : "test event3",
+            "startDate" : "10-24",
+            "endDate" : "12-5",
+            "category" : "some category",
+            "type" : "some type",
+            "description" : "blah blah blah",
+            "owner" : "Krebiehl, Alex",
+            "location" : "Dayton, OH",
+            "status" : "Active",
+            "registrations" : [
+                { "firstName": "Tyler", "lastname": "Darby", "id": 7},
+                { "firstName": "Jordan", "lastname": "Bossman", "id": 8},
+                { "firstName": "Tyler", "lastname": "Robertson", "id": 9}
+            ],
+            "food" : [
+                "Carrots",
+                "Peas"
+            ]
+        }];
+
+    $scope.displayEvent = function(eventId) {
+        $location.path('/event/view/' + eventId);
+    };
 
     }]);
 
